0c7ba226963145dd978105aa084caada1dd226ef
Kelvin Samuel Klutse <nextlinedesigns@gmail.com>
2023-06-25 09:52:46 +0000

    update

--- converter.js
+++ converter.js
@@ -34,6 +34,17 @@ fetch("https://openexchangerates.org/api/currencies.json")
     }
   });
   // fetch currencies automatically
+// fetch("https://openexchangerates.org/api/currencies.json")
+// .then(response => response.json())
+// .then(data => {
+//   const select = document.getElementById("receiveCurrency");
+//   for (const currencyCode in data) {
+//     const option = document.createElement("option");
+//     option.value = currencyCode;
+//     option.textContent = `${currencyCode} - ${data[currencyCode]}`;
+//     select.appendChild(option);
+//   }
+// });
 fetch("https://openexchangerates.org/api/currencies.json")
 .then(response => response.json())
 .then(data => {
@@ -44,7 +55,32 @@ fetch("https://openexchangerates.org/api/currencies.json")
     option.textContent = `${currencyCode} - ${data[currencyCode]}`;
     select.appendChild(option);
   }
+
+  // Get user's location using OpenCage Geocoder API
+  const apiKey = "e1cbe367b96f477fb85d1f352c31afbf";
+  const geocoder = new OpenCageApi({ key: apiKey });
+  geocoder.geocode({ q: "auto" }).then(response => {
+    const result = response.results[0];
+    const userCurrency = getCurrencyFromLocation(result.components.country_code);
+    select.value = userCurrency;
+  }).catch(error => {
+    console.error(error);
+  });
 });
+
+
+
+
+function getCurrencyFromLocation(countryCode) {
+  // You can modify this function to return the appropriate currency based on the user's country code
+  // For the sake of this example, let's assume the user's currency is USD if they're in the US, and EUR otherwise
+  if (countryCode === "US") {
+    return "USD";
+  } else {
+    return "EUR";
+  }
+}
+
 
 
 // Add event listeners to input fields
